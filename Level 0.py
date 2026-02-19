c326c7d44fd350eb53652f648fcd700fd453460d
kyraapaquette <kyraapaquette@gmail.com>
2026-02-19 10:47:35 -0800

    First Level of Over The Wire Commit

---
+++ Level 0.py
@@ -0,0 +1,18 @@
+$ ssh bandit0.labs.overthewire.org -p 2220
+
+bandit0@bandit.labs.overthewire.org's password: *******
+
+bandit0@bandit:~$ ls
+readme
+bandit0@bandit:~$ cat readme
+Congratulations on your first steps into the bandit game!!
+Please make sure you have read the rules at https://overthewire.org/rules/
+If you are following a course, workshop, walkthrough or other educational activity,
+please inform the instructor about the rules as well and encourage them to
+contribute to the OverTheWire community so we can keep these games free!
+
+The password you are looking for is: *******************************************************
+
+
+bandit0@bandit:~$ logout
+Connection to bandit.labs.overthewire.org closed.
